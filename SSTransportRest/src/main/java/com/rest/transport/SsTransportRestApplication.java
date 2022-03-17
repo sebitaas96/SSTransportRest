7effdfee8c362005b868dd7e7cb8217a9ad0df5f
sebas <sesirbu@gmail.com>
2022-03-17 10:42:10 +0100

    commi con proyecto limpio

---
+++ SSTransportRest/src/main/java/com/rest/transport/SsTransportRestApplication.java
@@ -0,0 +1,13 @@
+package com.rest.transport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsTransportRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsTransportRestApplication.class, args);
+	}
+
+}
